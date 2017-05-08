1f8aa7f7df647cfae7a35599dfa1f5ba0fe113f7
Mark Lee <crypto-hash@lazymalevolence.com>
2017-05-07 22:13:13 -0700

    Travis: run cargo fmt

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -6,6 +6,8 @@ set -e
 pip install 'travis-cargo<0.2' --user
 
 test -x $HOME/.cargo/bin/cargo-install-update || cargo install cargo-update
+test -x $HOME/.cargo/bin/rustfmt || cargo install rustfmt
+cargo install-update rustfmt
 
 if test "$TRAVIS_RUST_VERSION" = "nightly"; then
     test -x $HOME/.cargo/bin/cargo-clippy || cargo install clippy
--- ci/script.sh
+++ ci/script.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
-
-# the main build
-set -e
+#!/bin/bash -e
 
 travis-cargo build -- --target $TARGET
 travis-cargo test -- --target $TARGET
 
 if test "$TRAVIS_OS_NAME" = "linux" -a "$TARGET" = "x86_64-unknown-linux-gnu"; then
     travis-cargo --only stable doc
+
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+        cargo fmt -- --write-mode=diff $(git diff --name-only "$TRAVIS_COMMIT" "$TRAVIS_BRANCH" | grep \.rs$)
+    else
+        cargo fmt -- --write-mode=diff $(git show --format= --name-only "$TRAVIS_COMMIT_RANGE" | sort -u | grep \.rs$)
+    fi
 fi
 
 if test "$TRAVIS_RUST_VERSION" = "nightly"; then
