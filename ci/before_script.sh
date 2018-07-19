a88d5b3f47bf1c9936927ace46125a6f0b80e186
Mark Lee <crypto-hash@lazymalevolence.com>
2018-07-18 22:47:39 -0700

    Install clippy via rustup

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -4,7 +4,7 @@ rustup component add rustfmt-preview --toolchain=$TRAVIS_RUST_VERSION
 cargo install cargo-update || echo "cargo-update already installed"
 cargo install cargo-travis || echo "cargo-travis already installed"
 if test "$TRAVIS_RUST_VERSION" = "nightly"; then
-    cargo install clippy || echo "Clippy already installed"
+    rustup component add clippy-preview --toolchain=$TRAVIS_RUST_VERSION
 fi
 
 cargo install-update -a
