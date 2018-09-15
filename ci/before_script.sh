96824c1751d521a1b671e7b527893f4b7f03af04
Mark Lee <crypto-hash@lazymalevolence.com>
2018-09-14 21:00:18 -0700

    CI: Only run rustfmt/clippy on x86_64 linux stable

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 
-rustup component add rustfmt-preview --toolchain=$TRAVIS_RUST_VERSION
-cargo install cargo-update || echo "cargo-update already installed"
-cargo install cargo-travis || echo "cargo-travis already installed"
-if test "$TRAVIS_RUST_VERSION" = "nightly"; then
-    rustup component add clippy-preview --toolchain=$TRAVIS_RUST_VERSION
+if test "$TRAVIS_OS_NAME" = "linux" -a "$TARGET" = "x86_64-unknown-linux-gnu" -a "$TRAVIS_RUST_VERSION" = "stable"; then
+    rustup component add rustfmt-preview clippy-preview --toolchain=$TRAVIS_RUST_VERSION
 fi
 
+cargo install cargo-update || echo "cargo-update already installed"
+cargo install cargo-travis || echo "cargo-travis already installed"
 cargo install-update -a
--- ci/script.sh
+++ ci/script.sh
@@ -2,18 +2,14 @@
 
 cargo test --target $TARGET
 
-if test "$TRAVIS_OS_NAME" = "linux" -a "$TARGET" = "x86_64-unknown-linux-gnu"; then
-    if test "$TRAVIS_RUST_VERSION" = "stable"; then
-        cargo doc
-    fi
+if test "$TRAVIS_OS_NAME" = "linux" -a "$TARGET" = "x86_64-unknown-linux-gnu" -a "$TRAVIS_RUST_VERSION" = "stable"; then
+    cargo doc
 
     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
         cargo fmt -- --check $(git diff --name-only "$TRAVIS_COMMIT" "$TRAVIS_BRANCH" | grep \.rs$)
     else
         cargo fmt -- --check $(git show --format= --name-only "$TRAVIS_COMMIT_RANGE" | sort -u | grep \.rs$)
     fi
-fi
 
-if test "$TRAVIS_RUST_VERSION" = "nightly"; then
     cargo clippy --target $TARGET -- --allow clippy_pedantic
 fi
