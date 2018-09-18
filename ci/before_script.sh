6568bdd0dfbf595d602bd170ed7c40a7cbbdaa16
Mark Lee <crypto-hash@lazymalevolence.com>
2018-09-18 15:42:38 -0700

    CI: rework based on trust v0.1.2 template

--- ci/before_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-if test "$TRAVIS_OS_NAME" = "linux" -a "$TARGET" = "x86_64-unknown-linux-gnu" -a "$TRAVIS_RUST_VERSION" = "stable"; then
-    rustup component add rustfmt-preview clippy-preview --toolchain=$TRAVIS_RUST_VERSION
-fi
-
-cargo install cargo-update || echo "cargo-update already installed"
-cargo install cargo-travis || echo "cargo-travis already installed"
-cargo install-update -a
--- ci/script.sh
+++ ci/script.sh
@@ -1,15 +1,30 @@
 #!/bin/bash -xe
 
-cargo test --target $TARGET
+main() {
+    cross build --target $TARGET
+    cross build --target $TARGET --release
 
-if test "$TRAVIS_OS_NAME" = "linux" -a "$TARGET" = "x86_64-unknown-linux-gnu" -a "$TRAVIS_RUST_VERSION" = "stable"; then
-    cargo doc
-
-    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-        cargo fmt -- --check $(git diff --name-only "$TRAVIS_COMMIT" "$TRAVIS_BRANCH" | grep \.rs$)
-    else
-        cargo fmt -- --check $(git show --format= --name-only "$TRAVIS_COMMIT_RANGE" | sort -u | grep \.rs$)
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
     fi
 
-    cargo clippy --target $TARGET -- --allow clippy_pedantic
+    cross test --target $TARGET
+    cross test --target $TARGET --release
+
+    if test "$TARGET" = "x86_64-unknown-linux-gnu" -a "$TRAVIS_RUST_VERSION" = "stable"; then
+        cargo doc
+
+        if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+            cargo fmt -- --check $(git diff --name-only "$TRAVIS_COMMIT" "$TRAVIS_BRANCH" | grep \.rs$)
+        else
+            cargo fmt -- --check $(git show --format= --name-only "$TRAVIS_COMMIT_RANGE" | sort -u | grep \.rs$)
+        fi
+
+        cargo clippy --target $TARGET -- --allow clippy_pedantic
+    fi
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
 fi
