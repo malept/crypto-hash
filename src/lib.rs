09df4181003a54dc86c28f927f3eabbfd6356aa4
Mark Lee <crypto-hash@lazymalevolence.com>
2018-12-12 23:17:12 -0800

    Rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -46,11 +46,7 @@
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 extern crate commoncrypto;
 extern crate hex;
-#[cfg(not(any(
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "windows"
-)))]
+#[cfg(not(any(target_os = "macos", target_os = "ios", target_os = "windows")))]
 extern crate openssl;
 #[cfg(target_os = "windows")]
 extern crate winapi;
@@ -63,11 +59,7 @@ mod imp;
 #[cfg(target_os = "windows")]
 #[path = "imp/cryptoapi.rs"]
 mod imp;
-#[cfg(not(any(
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "windows"
-)))]
+#[cfg(not(any(target_os = "macos", target_os = "ios", target_os = "windows")))]
 #[path = "imp/openssl.rs"]
 mod imp;
 
