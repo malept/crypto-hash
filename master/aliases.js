bfbe3097dc82ca8c1a02180c63b4f111f52a73e7
Travis CI User <travis@example.org>
2018-12-20 21:58:19 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -32,6 +32,7 @@ ALIASES['crypto_hash'] = {};
 ALIASES['crypto_hash'] = {};
 ALIASES['crypto_hash'] = {};
 ALIASES['crypto_hash'] = {};
+ALIASES['crypto_hash'] = {};
 ALIASES['foreign_types'] = {};
 ALIASES['foreign_types'] = {};
 ALIASES['foreign_types'] = {};
