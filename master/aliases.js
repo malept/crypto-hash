123f0fa1fb91a6f9a9536300be5920e43fa996a4
Travis CI User <travis@example.org>
2018-12-27 06:20:13 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -2,6 +2,7 @@ var ALIASES = {};
 ALIASES['bitflags'] = {};
 ALIASES['cfg_if'] = {};
 ALIASES['crypto_hash'] = {};
+ALIASES['crypto_hash'] = {};
 ALIASES['foreign_types'] = {};
 ALIASES['foreign_types_shared'] = {};
 ALIASES['hex'] = {};
