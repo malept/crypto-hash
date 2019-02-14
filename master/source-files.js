1c118fb79b2e443addeced083c19b4185e0c9dd9
Travis CI User <travis@example.org>
2019-02-14 06:24:14 +0000

    Automatic Travis documentation build

--- master/source-files.js
+++ master/source-files.js
@@ -4,6 +4,7 @@ sourcesIndex['cfg_if'] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex['crypto_hash'] = {"name":"","dirs":[{"name":"imp","dirs":[],"files":["openssl.rs"]}],"files":["lib.rs"]};
 sourcesIndex['crypto_hash'] = {"name":"","dirs":[{"name":"imp","dirs":[],"files":["openssl.rs"]}],"files":["lib.rs"]};
 sourcesIndex['crypto_hash'] = {"name":"","dirs":[{"name":"imp","dirs":[],"files":["openssl.rs"]}],"files":["lib.rs"]};
+sourcesIndex['crypto_hash'] = {"name":"","dirs":[{"name":"imp","dirs":[],"files":["openssl.rs"]}],"files":["lib.rs"]};
 sourcesIndex['foreign_types'] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex['foreign_types_shared'] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex['hex'] = {"name":"","dirs":[],"files":["lib.rs"]};
