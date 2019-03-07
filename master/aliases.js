6434ddb21e66a0055fba0e0c1d021277619ba327
Travis CI User <travis@example.org>
2019-03-07 06:27:09 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -1,4 +1,14 @@
 var ALIASES = {};
+ALIASES["bitflags"] = {};
+ALIASES["cfg_if"] = {};
+ALIASES["crypto_hash"] = {};
+ALIASES["foreign_types"] = {};
+ALIASES["foreign_types_shared"] = {};
+ALIASES["hex"] = {};
+ALIASES["lazy_static"] = {};
+ALIASES["libc"] = {};
+ALIASES["openssl"] = {};
+ALIASES["openssl_sys"] = {};
 ALIASES['bitflags'] = {};
 ALIASES['bitflags'] = {};
 ALIASES['cfg_if'] = {};
