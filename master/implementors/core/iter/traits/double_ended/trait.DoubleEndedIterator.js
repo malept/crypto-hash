6283ed7b7999c03e3470392f4d3cba2ca77798dd
Travis CI User <travis@example.org>
2019-04-18 06:32:02 +0000

    Automatic Travis documentation build

---
+++ master/implementors/core/iter/traits/double_ended/trait.DoubleEndedIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["openssl"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"openssl/stack/trait.Stackable.html\" title=\"trait openssl::stack::Stackable\">Stackable</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/double_ended/trait.DoubleEndedIterator.html\" title=\"trait core::iter::traits::double_ended::DoubleEndedIterator\">DoubleEndedIterator</a> for <a class=\"struct\" href=\"openssl/stack/struct.IntoIter.html\" title=\"struct openssl::stack::IntoIter\">IntoIter</a>&lt;T&gt;",synthetic:false,types:["openssl::stack::IntoIter"]},{text:"impl&lt;'a, T:&nbsp;<a class=\"trait\" href=\"openssl/stack/trait.Stackable.html\" title=\"trait openssl::stack::Stackable\">Stackable</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/double_ended/trait.DoubleEndedIterator.html\" title=\"trait core::iter::traits::double_ended::DoubleEndedIterator\">DoubleEndedIterator</a> for <a class=\"struct\" href=\"openssl/stack/struct.Iter.html\" title=\"struct openssl::stack::Iter\">Iter</a>&lt;'a, T&gt;",synthetic:false,types:["openssl::stack::Iter"]},{text:"impl&lt;'a, T:&nbsp;<a class=\"trait\" href=\"openssl/stack/trait.Stackable.html\" title=\"trait openssl::stack::Stackable\">Stackable</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/double_ended/trait.DoubleEndedIterator.html\" title=\"trait core::iter::traits::double_ended::DoubleEndedIterator\">DoubleEndedIterator</a> for <a class=\"struct\" href=\"openssl/stack/struct.IterMut.html\" title=\"struct openssl::stack::IterMut\">IterMut</a>&lt;'a, T&gt;",synthetic:false,types:["openssl::stack::IterMut"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
