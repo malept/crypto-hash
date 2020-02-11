cb4654573a2bebc7fad678febef7f66491f10bc2
Mark Lee <crypto-hash@lazymalevolence.com>
2020-02-10 17:30:44 -0800

    build(ci): update cross git location

--- ci/install.sh
+++ ci/install.sh
@@ -30,7 +30,7 @@ install_compile_dependencies() {
     fi
 
     # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/rust-embedded/cross \
                        | cut -d/ -f3 \
                        | grep -E '^v[0.1.0-9.]+$' \
                        | $sort --version-sort \
@@ -38,7 +38,7 @@ install_compile_dependencies() {
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
-           --git japaric/cross \
+           --git rust-embedded/cross \
            --tag $tag \
            --target $target
 
